8bc75f9a676961e47a13ae58bbee07374dc8fed6
ana frescki <karolinefrescki@gmail.com>
2025-06-20 17:13:07 +0200

    updating

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// checking:
+console.log("Hello from JS");
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
